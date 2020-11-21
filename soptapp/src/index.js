ae41cc29659ca8d7eeab1ca8ce44f7658b6a82f6
hyundang <hyunjin5697@yonsei.ac.kr>
2020-11-21 19:11:53 +0900

    폴더...

--- soptapp/src/index.js
+++ soptapp/src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./modules";
+import {createStore} from 'redux';
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
